e2a95e637386ab63f2cc2f099446bc30a90a6582
Bartesto <barthuntley@iinet.net.au>
2016-05-06 09:38:16 +0800

    working loop need testing

--- create folder move file.R
+++ create folder move file.R
@@ -1,12 +1,36 @@
 ################################################################################
 ## Movin' files about
 
+rm(list=ls())
 
-wkdir <- ""
-newloc <- ""
+library(rgdal)
+library(dplyr)
+library(tidyr)
 
+### Directories
+topdir <- "Z:\\DEC\\Marine\\Working\\Kimberley\\Beaches\\Flight_Photos"
+photodir <- paste0(topdir, "\\Turtles\\Summer Aerial Surveys\\2013_2014\\",
+                   "ORIGINAL_PHOTOS\\Day_8_20140107")
+photodir <- paste0(topdir, "\\Turtles\\Summer Aerial Surveys\\2013_2014\\",
+                   "ORIGINAL_PHOTOS")
+mosdir <- paste0(topdir, "\\Turtles\\Summer Aerial Surveys\\2013_2014\\",
+                 "PHOTO_MOSAICS")
 
+### Get Info From Shape File
+setwd(topdir)
+shpfiles <- list.files(pattern = "*.shp")
+shpfiles <- shpfiles[!grepl("xml", shpfiles)] #xml handler
+shpnames <- unlist(strsplit(shpfiles, split = "\\."))
+shp <- shpnames[c(TRUE,FALSE)] #this returns the odd indexes
+shp <- readOGR(dsn = topdir, shp)
 
+### Make a Useful Data Frame
+shpdf <- shp@data
+shpdf$LOCATION <- as.character(shpdf$LOCATION)
+shpdf$IMAGE <- as.character(shpdf$IMAGE)
+shpdf$MOSFOLD <- paste(shpdf$LOCATION, shpdf$ID_MOSAIC, sep = "_")
+
+### Get Original Photo Folders to Loop Over
 list.dirs <- function(path=".", pattern=NULL, all.dirs=FALSE,
                       full.names=FALSE, ignore.case=FALSE) {
   # use full.names=TRUE to pass to file.info
@@ -19,20 +43,59 @@ list.dirs <- function(path=".", pattern=NULL, all.dirs=FALSE,
   else
     return(basename(dirs))
 }
+oFolds <- list.dirs(photodir)
 
-
-afiles <- list.dirs()
-folds <- substr(afiles, 8, 15)
-
-for(i in 1:length(folds)){
-  if(!file.exists(folds[i])){dir.create(folds[i])}
+for(i in 1:length(oFolds)){
+  photos <- list.files(paste(photodir, oFolds[i], sep = "\\"))
+  ## Subset Data Frame to Match Existing Photos
+  wkdf <- shpdf[photos %in% shpdf$IMAGE, ]
+  wkdf <- wkdf%>%
+    arrange(IMAGE)
+  ## Create Mosaic Folders
+  setwd(mosdir)
+  foldnames <- unique(wkdf$MOSFOLD)
+  for(j in 1:length(foldnames)){
+    if(!file.exists(foldnames[j])){dir.create(foldnames[j])}
+  }
+  ## Copy Photos to Mosaic Folders
+  for(k in 1:length(wkdf$IMAGE)){
+    from.k <- paste(photodir, wkdf$IMAGE[k], sep = "\\")
+    to.k <- paste(mosdir, wkdf$MOSFOLD[k], wkdf$IMAGE[k], sep = "\\")
+    file.copy(from=from.k, to=to.k, recursive = FALSE, overwrite = TRUE, 
+              copy.mode = TRUE)
+  }
 }
 
-for(j in 1:length(folds)){
-  dir.i <- paste0(dir,"\\", afiles[j])
-  from.i <- paste0(afiles[j], "\\", list.files(dir.i))
-  to.i <- folds[j]
-  file.copy(from=from.i, to=to.i, recursive = FALSE, overwrite = TRUE, copy.mode = TRUE)
+
+
+
+
+
+
+
+#
+photos <- list.files(photodir)
+wkdf <- shpdf[photos %in% shpdf$IMAGE, ]
+wkdf <- wkdf%>%
+  arrange(IMAGE)
+
+#
+setwd(mosdir)
+foldnames <- unique(wkdf$MOSFOLD)
+for(j in 1:length(foldnames)){
+  if(!file.exists(foldnames[j])){dir.create(foldnames[j])}
 }
+
+##
+for(k in 1:length(wkdf$IMAGE)){
+  from.k <- paste(photodir, wkdf$IMAGE[k], sep = "\\")
+  to.k <- paste(mosdir, wkdf$MOSFOLD[k], wkdf$IMAGE[k], sep = "\\")
+  file.copy(from=from.k, to=to.k, recursive = FALSE, overwrite = TRUE, 
+            copy.mode = TRUE)
+}
+
+### WATCH OUT!! Deleting Original Photos
+# files_to_delete <- paste(photodir, list.files(photodir), sep = "\\")
+# file.remove(files_to_delete)
 
 
